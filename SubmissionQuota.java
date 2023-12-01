4f43735a042b637f4bb6805d401e7286694924d0
Britney Patterson <124371187+TopRam3n@users.noreply.github.com>
2023-12-01 03:53:03 -0500

    Update SubmissionQuota.java
    Modified the design for the requirements. Added buttons for viewing submission quota and view submitted files for managing

--- SubmissionQuota.java
+++ SubmissionQuota.java
@@ -1,35 +1,47 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.nio.file.Path;
+
+
 
 public class SubmissionQuota extends JFrame {
     private int submissionQuota = 5; // Assign a fixed submission quota for each student
     private int submissionsMade = 0;
+    private ArrayList<FileInfo> submittedFiles; // To store the names of submitted files
 
     public SubmissionQuota() {
         setTitle("Submission Quota");
         setSize(400, 200);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(2, 1));
+        panel.setLayout(new GridLayout(3, 1));
 
         JButton cmdCheckQuota = new JButton("Check Submission Quota");
+        JButton cmdViewFiles = new JButton("View Submitted Files"); // New button for viewing submitted files
         JButton cmdCloseQuotaMenu = new JButton("Close Submission Quota Menu");
 
         panel.add(cmdCheckQuota);
+        panel.add(cmdViewFiles);
         panel.add(cmdCloseQuotaMenu);
 
         cmdCheckQuota.addActionListener(new CheckQuotaListener());
+        cmdViewFiles.addActionListener(new ViewFilesListener());
         cmdCloseQuotaMenu.addActionListener(new CloseQuotaMenuListener());
 
         add(panel);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setVisible(true);
+
+        // Load submitted files information from the FileInfo.txt
+        submittedFiles = loadFileInfo("FileInfo.txt");
     }
 
     private class CheckQuotaListener implements ActionListener {
@@ -45,11 +57,40 @@ public class SubmissionQuota extends JFrame {
         }
     }
 
+    private class ViewFilesListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            StringBuilder filesInfo = new StringBuilder("Submitted Files:\n");
+            for (FileInfo fileInfo : submittedFiles) {
+                filesInfo.append(fileInfo.getPath().getFileName()).append("\n");
+            }
+            JOptionPane.showMessageDialog(null, filesInfo.toString(), "Submitted Files", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
     private class CloseQuotaMenuListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             dispose(); // Close the submission quota menu
         }
+    }
+
+    private ArrayList<FileInfo> loadFileInfo(String idFile) {
+        ArrayList<FileInfo> files = new ArrayList<>();
+        try {
+            Scanner scan = new Scanner(new File(idFile));
+            while (scan.hasNext()) {
+                String line = scan.nextLine();
+                int id = Integer.parseInt(line.split(" ")[0]);
+                Path path = new File(line.split(" ")[1]).toPath();
+                FileInfo fInfo = new FileInfo(id, path);
+                files.add(fInfo);
+            }
+            return files;
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        return files;
     }
 
     public static void main(String[] args) {
